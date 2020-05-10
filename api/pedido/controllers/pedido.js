67d1b56994f8d522bbc07dfd7ccbb59a4f6b32a7
arturo <charturo.gil@gmail.com>
2020-05-10 14:29:10 -0500

    final

--- api/pedido/controllers/pedido.js
+++ api/pedido/controllers/pedido.js
@@ -1,4 +1,5 @@
 "use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
@@ -6,6 +7,62 @@
  */
 
 module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.pedido.search(ctx.query, [
+        "platillos_de_pedidos.platillo",
+      ]);
+    } else {
+      entities = await strapi.services.pedido.find(ctx.query, [
+        "platillos_de_pedidos.platillo",
+      ]);
+    }
+    entities = entities.map((entity) =>
+      sanitizeEntity(entity, {
+        model: strapi.models.pedido,
+        withPrivate: false,
+      })
+    );
+    entities.forEach((pedido) => {
+      pedido.platillos = pedido.platillos_de_pedidos;
+      pedido.platillos_de_pedidos = undefined;
+      pedido.platillos.forEach((platillo) => {
+        platillo.id = undefined;
+        platillo.pedido = undefined;
+        platillo.created_at = undefined;
+        platillo.updated_at = undefined;
+      });
+      pedido.created_at = undefined;
+      pedido.updated_at = undefined;
+    });
+
+    return entities;
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    console.log(id);
+    let pedido = await strapi.services.pedido.findOne({ id }, [
+      "platillos_de_pedidos.platillo",
+    ]);
+    pedido = sanitizeEntity(pedido, {
+      model: strapi.models.pedido,
+      withPrivate: false,
+    });
+    pedido.platillos = pedido.platillos_de_pedidos;
+    pedido.platillos_de_pedidos = undefined;
+    pedido.platillos.forEach((platillo) => {
+      platillo.id = undefined;
+      platillo.pedido = undefined;
+      platillo.created_at = undefined;
+      platillo.updated_at = undefined;
+    });
+    pedido.created_at = undefined;
+    pedido.updated_at = undefined;
+    return pedido;
+  },
+
   /**
    * Crea un pedido.
    *
@@ -14,15 +71,60 @@ module.exports = {
 
   async create(ctx) {
     let entity;
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      data.user = ctx.state.user.id;
-      entity = await strapi.services.pedido.create(data, { files });
-    } else {
-      ctx.request.body.user = ctx.state.user.id;
-      entity = await strapi.services.pedido.create(ctx.request.body);
+
+    ctx.request.body.user = ctx.state.user.id;
+    if (ctx.request.body.platillos === undefined) {
+      ctx.response.badRequest("Platillos is empty");
+      return;
     }
-    return sanitizeEntity(entity, { model: strapi.models.pedido });
+
+    const platillos = ctx.request.body.platillos;
+
+    if (typeof platillos !== "object") {
+      ctx.response.badRequest("Platillos is empty");
+      return;
+    }
+
+    const arrValidation = platillos.filter((platillo) => {
+      if (!platillo.id) return true;
+      if (!platillo.cantidad) return true;
+      if (platillo.comentarios !== undefined) {
+        if (typeof platillo.comentarios !== "string") return true;
+      }
+    });
+
+    if (arrValidation.length > 0) {
+      ctx.response.badRequest("Not valid");
+      return;
+    }
+
+    entity = await strapi.services.pedido.create(ctx.request.body);
+
+    const { id: pedido } = entity;
+    let platillos_de_pedido = [];
+    for (let i = 0; i < platillos.length; i++) {
+      const platillo = platillos[i];
+      const { id, comentarios, cantidad } = platillo;
+      const platillo_de_pedido = await strapi.services[
+        "platillos-de-pedido"
+      ].create({
+        pedido,
+        platillo: id,
+        comentarios,
+        cantidad,
+      });
+      platillo_de_pedido.id = platillo_de_pedido.platillo.id;
+      platillo_de_pedido.platillo = undefined;
+      platillo_de_pedido.pedido = undefined;
+      platillo_de_pedido.created_at = undefined;
+      platillo_de_pedido.updated_at = undefined;
+      platillos_de_pedido.push(platillo_de_pedido);
+    }
+
+    entity.user = undefined;
+    entity.platillos_de_pedidos = undefined;
+    entity.platillos = platillos_de_pedido;
+    return entity;
   },
 
   /**
--- api/platillo/controllers/platillo.js
+++ api/platillo/controllers/platillo.js
@@ -1,8 +1,56 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.platillo.search(ctx.query, [
+        "nombre",
+        "imagen",
+      ]);
+    } else {
+      entities = await strapi.services.platillo.find(ctx.query, [
+        "nombre",
+        "imagen",
+      ]);
+    }
+    entities = entities.map((entity) =>
+      sanitizeEntity(entity, {
+        model: strapi.models.platillo,
+        withPrivate: false,
+      })
+    );
+    return entities;
+  },
+
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.platillo.findOne({ id }, [
+      "nombre",
+      "imagen",
+    ]);
+    return sanitizeEntity(entity, {
+      model: strapi.models.platillo,
+      withPrivate: false,
+    });
+  },
+};
